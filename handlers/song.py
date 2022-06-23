9b5c1f5510e1c3ee2bc6a6aaf05b152525114a13
NANDAN✓ <89117179+NANDAN-VAMPIRE@users.noreply.github.com>
2022-06-23 15:15:05 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ :- 🎊 [ʙᴀᴅᴇ ʟᴏɢ🤧](https://t.me/Xmen_logon) 💖**"
+        rep = "**ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ :- 🎊 [ʙᴀᴅᴇ ʟᴏɢ🤧](https://t.me/BHUMIHAR_OP1) 💖**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("**🥲ʙᴇᴛᴀ ᴇʀʀᴏʀ ᴀʏᴀ💫 ᴘᴀᴘᴀ ᴋᴏ ʙᴜʟᴀᴏ 🤗 [ʙᴀᴅᴇ ʟᴏɢ🙄](https://t.me/Xmen_logon) 💖**")
+        m.edit("**🥲ʙᴇᴛᴀ ᴇʀʀᴏʀ ᴀʏᴀ💫 ᴘᴀᴘᴀ ᴋᴏ ʙᴜʟᴀᴏ 🤗 [ʙᴀᴅᴇ ʟᴏɢ🙄](https://t.me/BHUMIHAR_OP1) 💖**")
         print(e)
 
     try:
